c0a7d44be89a54ddea7022f795d50867ee138089
ByCat <31620096+itsbycat@users.noreply.github.com>
2025-06-20 23:26:33 +0300

    Add files via upload

---
+++ teleportfix/main.lua
@@ -0,0 +1,25 @@
+AddEventHandler("OnClientProcessUsercmds", function(event, playerid, cmds, numcmds, paused, margin)
+    local player = GetPlayer(playerid)
+
+    if not player or not player:IsValid() then return end
+
+    local pawn = player:CCSPlayerPawnBase()
+
+    if not pawn then return end
+
+    local viewAngles = pawn.EyeAngles
+
+    if not viewAngles then return end
+
+    if viewAngles.x > 89 or viewAngles.x < -89 then
+        ReplyToCommand(
+            playerid,
+            "HvH",
+            "Exploit açtığınız için öldürüldünüz!"
+        )
+
+        player:Kill()
+    end
+
+    return EventResult.Continue
+end)
