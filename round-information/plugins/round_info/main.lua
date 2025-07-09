7bfee4bf4f6594ef92a98f8a5b1183b4fad8b4c3
ByCat <31620096+itsbycat@users.noreply.github.com>
2025-07-09 12:55:59 +0300

    Update main.lua

--- round-information/plugins/round_info/main.lua
+++ round-information/plugins/round_info/main.lua
@@ -1,10 +1,27 @@
+local function GetNonFakePlayers()
+    local ret, activeClients = 0, playermanager:GetPlayerCap()
+
+    if activeClients == 0 then return ret end
+
+    for i = 0, activeClients do
+        player = GetPlayer(i)
+        if player and not player:IsFakeClient() then
+            ret = ret + 1
+        end
+    end
+
+    return ret
+end
+
 AddEventHandler("OnRoundStart", function(event)
     gameRules = GetCCSGameRules()
 
     map = server:GetMap()
-    totalPlayers = playermanager:GetPlayerCount()
+    totalPlayers = GetNonFakePlayers()
     maxPlayers = server:GetMaxPlayers()
     round = gameRules.TotalRoundsPlayed
+
+    if totalPlayers == 0 then return end
 
     playermanager:SendMsg(
         MessageType.Chat,
